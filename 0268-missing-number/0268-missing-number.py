5685db8703f13eaf39b88cea133921c15a3ca49c
Siun Park <52792088+uny-uny@users.noreply.github.com>
2022-11-11 10:19:10 -0600

    Time: 382 ms (23.48%), Space: 15.2 MB (80.95%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        
+        if nums[-1] != len(nums):
+            return len(nums)
+        
+        elif nums[0] != 0:
+            return 0
+        
+        for i in range(1, len(nums)):
+            expected_num = nums[i-1] + 1
+            if nums[i] != expected_num:
+                return expected_num
